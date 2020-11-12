e2ee0919843a5d9b707a27e4c5177299ed749427
Adam Rudny <rudnyadam@gmail.com>
2020-11-12 18:13:15 +0900

    Update 3
    Added:
    Direction functionality

--- snake.js
+++ snake.js
@@ -7,8 +7,8 @@ const box = 32;
 // Load Images
 const ground = new Image();
 ground.src = "img/ground.png";
-const food = new Image();
-food.src = "img/food.png";
+const foodImg = new Image();
+foodImg.src = "img/food.png";
 
 // Create the Snake
 let snake = [];
@@ -23,21 +23,68 @@ let food = {
     y : Math.floor(Math.random()*15+3) * box
 };
 
-const score = 0;
-// Create the score var
-let score = score;
 
-// Control Snake
-let d; 
+// Create the score var
+let score = 0;
+
+// Control the Snake
+let d;
+
+document.addEventListener("keydown",direction);
+
+function direction(event) {
+    let key = event.keyCode;
+    if (key == 37 && d != "RIGHT") {
+        d = "LEFT";
+    } else if (key == 38 && d != "DOWN") {
+        d = "UP";
+    } else if (key == 39 && d != "LEFT") {
+        d = "RIGHT";
+    } else if (key == 40 && d != "UP")
+        d = "DOWN";
+}
 
 // Draw Everything to the Canvas
-
 function draw (){
 
-    ctx.drawImage(ground,0,0);  
-    
+    ctx.drawImage(ground,0,0);
+
+    for (let i = 0; i < snake.length ; i++){
+        ctx.fillStyle = ( i == 0)? "green" : "dark-green";
+        ctx.fillRect(snake[i].x,snake[i].y,box,box);
+
+        ctx.strokeStyle = "black";
+        ctx.strokeRect(snake[i].x,snake[i].y,box,box);
+    }
+
+    ctx.drawImage(foodImg, food.x, food.y);
+
+    // Old Head Position
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    // Remove the Tail
+    snake.pop();
+
+    // Direction
+    if( d == "LEFT") snakeX -= box;
+    if( d == "UP") snakeY -= box;
+    if( d == "RIGHT") snakeX += box;
+    if( d == "DOWN") snakeY += box;
+
+    // Add New Head
+
+    let newHead = {
+        x : snakeX,
+        y : snakeY
+    }
+
+    snake.unshift(newHead);
+
+    ctx.fillStyle = "white";
+    ctx.font = "45px Changa one";
+    ctx.fillText(score, 2*box, 1.6*box);
 }
 
 // Call draw function every 100ms 
-
 let game = setInterval(draw,100);
